9ac9619325a292760af3df01dbc76b56d9f7c838
gabrielgiroe1 <gabriel.giroe@gmail.com>
2022-01-02 12:22:32 +0200

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -24,10 +24,17 @@ ActiveRecord::Schema.define(version: 2021_12_26_134559) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at", precision: 6
     t.datetime "remember_created_at", precision: 6
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at", precision: 6
+    t.datetime "last_sign_in_at", precision: 6
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "name"
     t.string "username"
+    t.index "\"confirmation_token\"", name: "index_users_on_confirmation_token", unique: true
+    t.index "\"unlock_token\"", name: "index_users_on_unlock_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
