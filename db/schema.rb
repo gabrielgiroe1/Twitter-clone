7de535f9fe95ec3c37a2783ea1fb5c61aa559e67
gabrielgiroe1 <gabriel.giroe@gmail.com>
2022-01-04 11:14:10 +0200

    Edit schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_26_134559) do
+ActiveRecord::Schema.define(version: 2022_01_02_131009) do
 
   create_table "tweets", force: :cascade do |t|
     t.text "tweet"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -33,8 +34,6 @@ ActiveRecord::Schema.define(version: 2021_12_26_134559) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "name"
     t.string "username"
-    t.index "\"confirmation_token\"", name: "index_users_on_confirmation_token", unique: true
-    t.index "\"unlock_token\"", name: "index_users_on_unlock_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
